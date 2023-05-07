f282cff164d1069676439bae91a6e059906c023d
stjn <ole.yves@gmail.com>
2023-05-07 14:25:16 +0400

    Attempt to fix errors in EventStreams#SetData

--- DiscordWikiBot/EventStreams.cs
+++ DiscordWikiBot/EventStreams.cs
@@ -509,7 +509,7 @@ namespace DiscordWikiBot
 				var key = item.Key;
 				var value = item.Value;
 				var requiredValueType = allowedKeys.GetValueOrDefault(key)?.GetType();
-				if (requiredValueType == null) {
+				if (item.Value == null || requiredValueType == null) {
 					continue;
 				}
 
@@ -519,12 +519,12 @@ namespace DiscordWikiBot
 				}
 
 				// Ignore default values if not resetting
-				if (discardPrevData == false && result.Property(key).Equals(value)) {
+				if (discardPrevData == false && result?.Property(key)?.Equals(value)) {
 					continue;
 				}
 
 				// Reset to default
-				if (allowedKeys.GetValueOrDefault(item.Key).Equals(item.Value)) {
+				if (allowedKeys.GetValueOrDefault(item.Key)?.Equals(item.Value)) {
 					if (discardPrevData == true) {
 						JProperty prop = result.Property(key);
 						if (prop != null)
